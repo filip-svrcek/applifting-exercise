a63c35731345c0866aa650dc98c1954a3e35de8b
filip-svrcek <svrcek@cngroup.dk>
2025-05-18 14:27:49 +0200

    JwtModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+      global: true,
+    }),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
 })
