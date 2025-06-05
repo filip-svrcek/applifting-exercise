9289cb243d1e183357fc3fd9cf7045eb4826865b
filip-svrcek <svrcek@cngroup.dk>
2025-06-05 18:40:11 +0200

    graphql added

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,24 @@ import { AuthModule } from './auth/auth.module';
 import { ArticlesModule } from './articles/articles.module';
 import { CommentsModule } from './comments/comments.module';
 import { VotesModule } from './votes/votes.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
-  imports: [PrismaModule, UsersModule, AuthModule, ArticlesModule, CommentsModule, VotesModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: true,
+      path: '/graphql',
+    }),
+    PrismaModule,
+    UsersModule,
+    AuthModule,
+    ArticlesModule,
+    CommentsModule,
+    VotesModule,
+  ],
 })
 export class AppModule {}
